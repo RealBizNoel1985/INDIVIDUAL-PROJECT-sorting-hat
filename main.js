478245bac9d385a28f3ca33b3a0886a4d7f132d8
Lucas Noel <lucasnoel1985@gmail.com>
2024-01-31 14:05:33 -0600

    Seeking Guidance

--- main.js
+++ main.js
@@ -1,195 +1,162 @@
-let filterToggle = true;
-
-//Create the Array and name it//
-const students = [
-
- //add 28 objects 
-
-{
-    id: 1,
-    name: "Jim",
-    type: "",
-    age: "",
-    power: "",
-    house: "Gryffindor",
-    imageUrl: "https://www.gameinformer.com/sites/default/files/styles/full/public/2021/12/17/aa464761/bully-header-image.jpg"
-   
-},
-
-{
-    id: 2,
-    name: "Cher",
-    type: "",
-    age: "",
-    power: "",
-    house: "Hufflepuff",
-    imageUrl: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7727cf71-88d3-47e7-aac7-c10a64cbc5d3/dfy4x0l-b0a5a4f5-16e9-4fb9-bb05-355e72e0119b.png/v1/fill/w_1280,h_854,q_80,strp/cher_horowitz_by_darksealstudios_dfy4x0l-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODU0IiwicGF0aCI6IlwvZlwvNzcyN2NmNzEtODhkMy00N2U3LWFhYzctYzEwYTY0Y2JjNWQzXC9kZnk0eDBsLWIwYTVhNGY1LTE2ZTktNGZiOS1iYjA1LTM1NWU3MmUwMTE5Yi5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.Y8k-6KlVHR-a9z7KbkZS7DMFjFNlUwjchEmw3fNRRvU"
-
-},
-
-{
-    id: 3,
-    name: "Carlton",
-    type: "",
-    age: "",
-    power: "",
-    house: "Ravenclaw",
-    imageUrl: "https://everythingwasbetter.com/wp-content/uploads/2023/03/fresh-prince-of-bel-air-outfits-prep-style-carlton-banks.jpg"
-   
-},
-
-{
-    id: 4,
-    name: "Abigail",
-    type: "",
-    age: "",
-    power: "",
-    house: "Slytherin",
-    imageUrl: "https://www.myfilmviews.com/wp-content/uploads/2013/09/thebighit2.png.png"
-   
-},
 
 
-];
-//render all the info based on the id of each product card//
-const renderToDom = (divId, htmlToRender) => {
-    const selectedDiv = document.querySelector(divId);
-    selectedDiv.innerHTML = htmlToRender;
-  };
-  
-  //get the cards on the DOM//
-  const cardsOnDom = (array) => {
-    let domString = "";
 
- //add the for of loop//
- for (const student of array) {
-  domString += `<div class="card" style="width: 18rem;">
-  <img src=${student.imageUrl} class="card-img-top" alt="...">
-  <div class="card-body">
-  <p class="card-text">Name: ${student.name}</p>
-  <p class="card-text">Type: ${student.type}</p>
-  <p class="card-text">Age: ${student.age}</p>
-  <p class="card-text">Power: ${student.power}</p>
-  <p class="card-text">House: ${student.house}</p>
-  <button type="button" class="btn btn-danger" id="delete--${student.id}">Expel</button>
-  </div>
-</div>`;
+const classPlacement = ['Gryffindor', 'Hufflepuff', 'Ravenclaw','Slytherin']
+const students = []
+const expelledStudents = []
+
+
+function showDiv() {
+  document.getElementById('welcomeDiv').style.display = "block";
 }
-  
-//render to Dom for the app
-    renderToDom("#app", domString);
-  };
-  
-  //create a query selector for the button filters
-  const showAllButton = document.querySelector("#All");
-  const showGryffindorButton = document.querySelector("#Gryffindor");
-  const showHufflepuffButton = document.querySelector("#Hufflepuff");
-  const showRavenclawButton = document.querySelector("#Ravenclaw");
-  const showSlytherinButton = document.querySelector("#Slytherin");
-
-//create the function for filter Toggle//
-  const filter = (typeString) => {
-    filterToggle = false;
-    filterToggle = true;
-    const typeArray = [];
-  
-    for (const member of students) {
-      if (member.type === typeString) {
-        typeArray.push(member);
-      }
-    }
-  //create the cardsOnDom for the type array
-    cardsOnDom (typeArray);
-  };
-  
-  
-  
-  //show buttons and add the event listeners
-  showAllButton.addEventListener("click", (e) => {
-    filterToggle = true;
-    cardsOnDom(students);
-  });
-  
-  showGryffindorButton.addEventListener("click", (e) => {
-     filter("Gryffindor");
-  });
-  
-  showHufflepuffButton.addEventListener("click", (e) => {
-    filter("Hufflepuff");
-  });
-  
-  showRavenclawButton.addEventListener("click", (e) => {
-    filter("Ravenclaw");
-  });
-
-  showSlytherinButton.addEventListener("click", (e) => {
-    filter("Slytherin");
-  });
-
-//create the query selector for the form to use 
-  const form = document.querySelector('form');
 
 
-   //add the create a new student event listener
-  const createNewStudent = (e) => {
-    e.preventDefault(); // EVERY TIME YOU CREATE A FORM
-  
-  //add the info values for the new student
-  const newStudentObj = {
-    id: students.length + 1,
-    name: document.querySelector("#name").value,
-    type: document.querySelector("#type").value,
-    age: document.querySelector("#age").value,
-    power: document.querySelector("#power").value,
-    house: document.querySelector("#house").value,
-    imageUrl: document.querySelector("#image").value
-    }
+const renderToDom = (array) => {
 
-  //push the new Student to the DOM
-  students.push(newStudentObj);
+  let domString = ""
+
+array.map((object, index) => {
+  domString += 
+      `<div id="newStudent" class="card mb-3"  style="max-width: 340px;">
+        <div class="row g-0">
+          <div class="col-md-4" id="${object.house}">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body" >
+              <h5 class="card-titles" id ="nameCard">${object.name}</h5>
+              <p class="card-text">${object.house.toUpperCase()}</p>
+              <button type ="button" class="btn btn-danger" id="expel--${index}" >EXPEL</button>
+            </div>
+          </div>
+        </div>
+      </div>`
+    })
+    app.innerHTML = domString
+}
+
+const showAllButton = document.querySelector("#All");
+const showGryffindorButton = document.querySelector("#Gryffindor");
+const showHufflepuffButton = document.querySelector("#Hufflepuff");
+const showRavenclawButton = document.querySelector("#Ravenclaw");
+const showSlytherinButton = document.querySelector("#Slytherin");
+
+
+showAllButton.addEventListener("click", (e) => {
   cardsOnDom(students);
-  form.reset();
-}
+});
 
-  //add event listener for the new student
-  form.addEventListener('submit', createNewStudent);
+showGryffindorButton.addEventListener("click", (e) => {
+   filter("Gryffindor");
+});
 
+showHufflepuffButton.addEventListener("click", (e) => {
+  filter("Hufflepuff");
+});
+
+showRavenclawButton.addEventListener("click", (e) => {
+  filter("Ravenclaw");
+});
+
+showSlytherinButton.addEventListener("click", (e) => {
+    filter("Slytherin");
+});
+
+
+const form = document.querySelector("form")
+
+form.addEventListener('submit',  (e) => {
+  e.preventDefault()
   
- //add the query selector for the app
-  const app = document.querySelector("#app");
+    const newStudentObj ={
+      id: students.length + 1,
+      name: document.querySelector("#studentName").value,
+      house: classPlacement[Math.floor(Math.random() * classPlacement.length)],
+      studentId: Math.floor(Math.random() * 100)  + 1
+      //studentId: Math.floor(Math.random() * (999 - 100) ) + 100
+      }
+
+    students.push(newStudentObj)
+    renderToDom(students)
+    form.reset()
+
+  })
 
 
-   //add the query selector that returns the element
-  app.addEventListener('click', (e) => {
- 
-  
-  //add the target id for the delete
-  if (e.target.id.includes("delete")) {
-    const [, id] = e.target.id.split("--");
-    
-  
-  const index = students.findIndex(e => e.id === Number(id));
-  
-  const student = students.find((p) => p.id === Number(id))
+const pit = document.querySelector("#pit")
 
-  students.splice(index, 1);
+const renderToPit = (array) => {
+  let domStringPit = ""
 
-  
+  array.map((object, index) => {
+    domStringPit += 
+        `<div id="newstudent" class="card m-1" style="width: 15rem;" id="${index}">
+        <img src="https://www.looper.com/img/gallery/harry-potter-all-the-villains-ranked-from-bad-to-diabolical/l-intro-1678283151.jpg" alt="Team">
+        <div class="card-body text-center">
+          <p class="h6 card-text text-dark">Unfortunately... <span class="text-dark">${object.name}</span>, is no longer a student!</p>
+        </div>
+      </div>`
+      })
+      pit.innerHTML = domStringPit
+  }
+
+
+
+
+
+const expelStudent = document.querySelector("#app")
+
+expelStudent.addEventListener("click", (e) => {
+  if(e.target.id.includes("expel")){
+    const [,index] = e.target.id.split("--");
+      expelledStudents.push(students[index])
+      students.splice(index, 1)
+    renderToDom(students)
+    renderToPit(expelledStudents)
+  }
+})
+
+
+const filter = (house) => {
+  const filteredArray = []
+
+  this.studentList.forEach((studentList, i) => {
+    studentList.checked = this.selectedStudents.includes(i)
+  })
+      if(object.house === house){
+        filteredArray.push(object)
+    }
+    renderToDom("#app", filteredArray)
+  }
+
+
+
+const filterButtons = document.querySelector("#filter-buttons")
+filterButtons.addEventListener("click", (e) => {
+  console.log("click")
+  const id = e.target.id 
+  const possibleHouses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+  if(id === "all"){
+    renderToDom(students)
+  } else if(possibleHouses.includes(id)){
+    filter(id)
+  }
+
   if (filterToggle) {
-    cardsOnDom(students) 
+    cardsOnDom(pets) 
   } else {
   
-    //use proper filtering selection
-    filter(student.house);
+  filter(house.type);
   }
-  
-  }
-  });
-  
-  const startApp = () => {
-    cardsOnDom(students);
-  };
-  startApp();
-   
-  console.log ("Hello World");
-  
+});
+
+
+
+
+const startApp = () => {
+
+  renderToDom(students)
+  events()
+}
+
+startApp()
